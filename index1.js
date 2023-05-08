605bf3c7027730cb393b489fe24203c1ed84cb3b
nbathe <97980762+nbathe@users.noreply.github.com>
2023-05-08 23:05:40 +0530

    Add files via upload

---
+++ index1.js
@@ -0,0 +1,27 @@
+
+function c(val)
+{
+document.getElementById("display").value=val;
+}
+function a(val)
+{
+document.getElementById("display").value+=val;
+}
+function e() 
+{ 
+    try 
+    { 
+        c(eval(document.getElementById("display").value)) 
+    } 
+    catch(e) 
+    {
+        c('Error') 
+    } 
+}  
+
+
+
+
+
+
+
